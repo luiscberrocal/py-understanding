e2ccf492dafec82bdbfb27ba64da13626abb0ec0
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-06-06 21:19:07 -0500

    Documenting the use of enums

---
+++ python_libs/pydantic/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class VendorType(str, Enum):
+    PERSON = 'PERSON'
+    COMPANY = 'COMPANY'
--- python_libs/pydantic/models.py
+++ python_libs/pydantic/models.py
@@ -4,11 +4,14 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
+from python_libs.pydantic.enums import VendorType
+
 
 class Vendor(BaseModel):
     name: str = Field(description='Name of the vendor.', max_length=64)
     national_id: str = Field(description='National id of the vendor.', max_length=32, default='NOT SUPPLIED')
     verification_digit: Optional[str] = Field(description='Verification digit of the National Id.', max_length=2)
+    vendor_type: VendorType = Field(description='Type of vendor i.e. person or company.', default=VendorType.COMPANY)
 
 
 class Receipt(BaseModel):
