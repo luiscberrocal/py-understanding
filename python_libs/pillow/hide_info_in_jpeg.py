233a4f0fe00a4c79fd10c29d3b39c3541ec5f72a
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-07-06 05:34:27 -0700

    Hide data in jpeg

--- python_libs/pillow/hide_info_in_jpeg.py
+++ python_libs/pillow/hide_info_in_jpeg.py
@@ -6,5 +6,7 @@ def add_string_to_jpeg(image: Path, text: str):
         f.write(text.encode('utf-8'))
 
 
-
-
+if __name__ == '__main__':
+    photo = Path(__file__).parent / 'download.jpeg'
+    message = 'This is hidden'
+    add_string_to_jpeg(photo, message)
