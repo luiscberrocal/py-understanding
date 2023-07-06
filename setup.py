7222a490ee7ee02b5898fce167734bcd3ce27fd6
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-07-06 05:33:56 -0700

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='python_libs',
-    version='0.4.0',
+    version='0.5.0',
     description="""Testt""",
     long_description="Long description",
     author='Luis Carlos Berrocal',
