bed20ef1ced023e2f9c9ff33dc7446e67c193234
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-07-15 09:06:39 -0500

    Mailer

--- python/sending_emails/gmail.py
+++ python/sending_emails/gmail.py
@@ -1,7 +1,50 @@
+import os
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from python.sending_emails.enums import EmailFormat
+from python.sending_emails.models import EmailMessage
 
 
 class MailServer:
 
     def __init__(self, sender_email: str, password: str):
-        pass
+        self.sender_email = sender_email
+        self.password = password
 
+    def send_email(self, email_message: EmailMessage):
+        # The mail addresses and password
+        # Setup the MIME
+        message = MIMEMultipart()
+        message['From'] = self.sender_email
+        message['To'] = ', '.join(email_message.recipients)
+        message['Subject'] = email_message.subject
+        # The subject line
+        # The body and the attachments for the mail
+        if email_message.format == EmailFormat.HTML:
+            message.add_header('Content-Type', 'text/html')
+        message.attach(MIMEText(email_message.content, email_message.format.value))
+        if email_message.attachments is not None:
+            for attachment in email_message.attachments:
+                attach_file = open(attachment, 'rb')  # Open the file as binary mode
+                payload = MIMEBase('application', 'octate-stream')
+                payload.set_payload(attach_file.read())
+                encoders.encode_base64(payload)  # encode the attachment
+                # add payload header with filename
+                fn = os.path.basename(attachment)
+                payload.add_header(
+                    "Content-Disposition",
+                    f"attachment; filename= {fn}",
+                )
+                message.attach(payload)
+        # Create SMTP session for sending the mail
+        session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+        session.starttls()  # enable security
+        session.login(self.sender_email, self.password)  # login with mail_id and password
+        text = message.as_string()
+        senders_response = session.sendmail(self.sender_email, email_message.recipients, text)
+        session.quit()
+        return senders_response
---
+++ python/sending_emails/models.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+from .enums import EmailFormat
+
+
+class SenderConfig(BaseModel):
+    email: str
+    password: str
+
+
+class EmailMessage(BaseModel):
+    recipients: List[str]
+    subject: str
+    content: str
+    attachments: Optional[List[Path] | None]
+    format: EmailFormat = Field(default=EmailFormat.TEXT)
