d79f69e1de7b517cbba7d722a80bcebb25c0fd24
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-07-25 12:09:33 -0500

    Parsing dates

--- python_libs/python_dateutil/parsing.py
+++ python_libs/python_dateutil/parsing.py
@@ -3,10 +3,26 @@ from datetime import datetime
 from dateutil.parser import parse
 
 
-def parse_jira_date(date_str:str) -> datetime:
+def parse_jira_date(date_str: str) -> datetime:
     dt = parse(date_str)
     return dt
 
 
+def format_date(value: str):
+    date_format = '%d/%b/%Y %I:%M %p'
+    try:
+        dt = datetime.strptime(value, date_format)
+        return dt
+    except Exception as e:
+        print(f'{value}')
+        print(f'{e}')
+        raise e
+
+
 if __name__ == '__main__':
-    jira_date = ''
+    jira_date_str = '25/Jul/23 2:23 PM'
+    jira_date = parse_jira_date(jira_date_str)
+    print(jira_date)
+
+    # This will raise an error because the hour need to be zero padded.
+    jira_date = format_date(jira_date_str)
