561f8c771b1b1632585d19aa8a0cd322ddf916d5
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-08-02 17:33:01 -0500

    Zipping files

---
+++ python/zip/compressing.py
@@ -0,0 +1,17 @@
+import zipfile
+from pathlib import Path
+
+
+def unzip_file(zip_file: Path, target_folder: Path, password: str):
+    # How to Extract Files from a Password Protected Zip File
+
+    with zipfile.ZipFile(zip_file, 'r') as zip:
+        zip.extractall(target_folder, pwd=password.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    output_folder = Path(__file__).parent.parent.parent / 'output'
+    # zip -P pass123 ccat-command.zip ./davinci_resolve/*.*
+    z_file = Path('compressed_folder_pwd.zip')
+    pwd = 'pass123'
+    unzip_file(zip_file=z_file, target_folder=output_folder, password=pwd)
