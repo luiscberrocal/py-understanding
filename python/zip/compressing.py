f64e02a05f353ef8258987806fb11e9636c8bd6d
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-08-02 21:37:13 -0500

    Compressing

--- python/zip/compressing.py
+++ python/zip/compressing.py
@@ -2,14 +2,14 @@ import zipfile
 from pathlib import Path
 
 
-def unzip_file(zip_file: Path, target_folder: Path, password: str):
+def unzip_all(zip_file: Path, target_folder: Path, password: str):
     # How to Extract Files from a Password Protected Zip File
 
     with zipfile.ZipFile(zip_file, 'r') as zip:
         zip.extractall(target_folder, pwd=password.encode('utf-8'))
 
 
-def unzip_file_independent(zip_file: Path, target_folder: Path, password: str):
+def unzip_file(zip_file: Path, target_folder: Path, password: str):
     with zipfile.ZipFile(zip_file, 'r') as zip:
         files = zip.namelist()
         for file in files:
@@ -23,5 +23,5 @@ if __name__ == '__main__':
     z_file = Path('compressed_folder_pwd.zip')
     pwd = 'pass123'
     # unzip_file(zip_file=z_file, target_folder=output_folder, password=pwd)
-    unzipped_files = unzip_file_independent(zip_file=z_file, target_folder=output_folder, password=pwd)
+    unzipped_files = unzip_file(zip_file=z_file, target_folder=output_folder, password=pwd)
     print(unzipped_files)
