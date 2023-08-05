a98ca69c9b6920b3b3c3fe5994a35df5b7dd6322
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-08-05 11:08:06 -0500

    Cleanup

--- python/path_lib/cleanup.py
+++ python/path_lib/cleanup.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import datetime
 
 from pathlib import Path
@@ -8,21 +9,26 @@ def format_size(size: float) -> str:
     return f'{mb:.2f} MB'
 
 
-def clean(folder: Path, glob_pattern: str):
+def clean(folder: Path, glob_pattern: str, max_age: int = None):
     files = folder.glob(glob_pattern)
     files_to_delete = []
     for i, file in enumerate(files, 1):
         creation_time = file.stat().st_ctime
-        creation_date = datetime.fromtimestamp(creation_time)
-        size = format_size(file.stat().st_size)
-        content = f'{i} {file} size: {size} creation: {creation_date}'
-        print(content)
-        delete_file = input('Delete?')
-        if delete_file.upper() == 'Y':
-            file.unlink()
+        age_days = (datetime.now() - datetime.fromtimestamp(creation_time)).total_seconds() / 3600.0 / 24.0
+        if max_age is None:
+            max_age = 1_000_000
+        if age_days <= max_age:
+            size = format_size(file.stat().st_size)
+            content = f'{i} {file} size: {size} age: {age_days:.1f} days old'
+            print(content)
+            action = input('Action [D]elete, [S]top [N]one')
+            if action.upper() == 'D':
+                file.unlink()
+            elif action.upper() == 'S':
+                sys.exit()
 
 
 if __name__ == '__main__':
     downloads = Path.home() / 'Downloads'
     g_pattern = '**/*.pdf'
-    clean(folder=downloads, glob_pattern=g_pattern)
+    clean(folder=downloads, glob_pattern=g_pattern)  # , max_age=60)
