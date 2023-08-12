abf6570fc59266ebabb77dee05c9bfbbe131171f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-08-12 08:27:40 -0500

    Pydantic

---
+++ python_libs/pydantic/model_config.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from typing import Any, Optional
+
+from pydantic import BaseModel, Field
+
+# https://docs.pydantic.dev/1.10/usage/model_config/
+class Data(BaseModel):
+    auth: str = Field(default='00000000')
+    store: str = Field(default='NOSTORE')
+    ticket: str
+    partial: str = 'T'
+    metadata: Any = None
+    client_name: str = Field(..., alias='clientName')
+    message_ticket: str = Field(alias='messageTicket', default='Sent to FE via script')
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class Payload(BaseModel):
+    data: Optional[Data] = None
+    type: str = 'PAYMENT'
+    folio: Optional[str] = None
+    amount: float = None
+    account: Optional[str] = None
+    event_date: Optional[str] = Field(default='0', alias='eventDate')
+    reference: Optional[str] = None
+
+    class Config:
+        allow_population_by_field_name = True
+
