79aba108ced348cb3939fadee456d630ca75f01f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-08-17 10:04:33 -0500

    Timezones

--- python_libs/pytz/timezones.py
+++ python_libs/pytz/timezones.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 import pytz
 
@@ -45,8 +46,27 @@ def convert_to_utc():
     print(f'UTC   timestamp for Colombia: {int(utc_date.timestamp())}')
     print(f'-0500 timestamp for Colombia: {int(pty_aware_date.timestamp())}')
 
+def datetime_created_with_timezone():
+    """
+    Do not user pytz.timezone to create a datetime use ZoneInfo.
+    """
+    datetime_format = '%Y-%m-%d %H:%M:%S %Z%z'
+    pty_timezone = pytz.timezone('America/Panama')
+    pty_aware_date = datetime(2023, 3, 22, 13, 15, tzinfo=pty_timezone)
+    pty_aware_date2 = datetime(2023, 3, 22, 13, 15, tzinfo=ZoneInfo('America/Panama'))
+
+    print(f'Now -0500 date for Colombia: {pty_aware_date.strftime(datetime_format)}')
+    print(f'ISO Format {pty_aware_date.isoformat()}')
+
+    print(f'Now -0500 date for Colombia: {pty_aware_date2.strftime(datetime_format)}')
+    print(f'ISO Format {pty_aware_date2.isoformat()}')
+
 
 if __name__ == '__main__':
     # add_timezone_to_naive_date()
     # using_datetime_now()
-    convert_to_utc()
+    # convert_to_utc()
+
+    datetime_created_with_timezone()
+
+
