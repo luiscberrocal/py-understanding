0a2bc68840588ec2424d4e55016ad5cc12eca7a5
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-08-30 16:56:16 -0500

    Half baked issue with inheritance.

--- python_libs/pydantic/enums.py
+++ python_libs/pydantic/enums.py
@@ -10,3 +10,18 @@ class Country(str, Enum):
     PANAMA = 'PA'
     COLOMBIA = 'CO'
     MEXICO = 'MX'
+    
+
+class FieldType(str, Enum):
+    """Supported fields."""
+    BOOLEAN_FIELD = 'BooleanField'
+    CHAR_FIELD = 'CharField'
+    COUNTRY_FIELD = 'CountryField'
+    DATE_FIELD = 'DateField'
+    DATETIME_FIELD = 'DateTimeField'
+    DECIMAL_FIELD = 'DecimalField'
+    FOREIGNKEY = 'ForeignKey'
+    GENERIC_IP_ADDRESS_FIELD = 'GenericIPAddressField'
+    INTEGER_FIELD = 'IntegerField'
+    MONEY_FIELD = 'MoneyField'
+    TEXT_FIELD = 'TextField'
