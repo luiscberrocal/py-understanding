ef5f86663fcf4b0e5e6a1d3b57b55346b0e8f17a
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-09-01 10:44:30 -0500

    Added mixins

---
+++ python_libs/sqlalchemy/db/db_setup.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://gwen@localhost/fast_lms"
+SQLITE_FILE = Path(__file__).parent / 'requirements_db.sqlite'
+SQLALCHEMY_DATABASE_URL = f"sqlite://{SQLITE_FILE}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+
+Base = declarative_base()
+
+
+# DB Utilities
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
