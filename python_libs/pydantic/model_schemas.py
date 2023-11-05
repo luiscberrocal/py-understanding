199272b57f88cfc0b4414b78e75b986e8f73018c
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-05 11:33:04 -0500

    Testing schemas

--- python_libs/pydantic/model_schemas.py
+++ python_libs/pydantic/model_schemas.py
@@ -1,3 +1,5 @@
+import json
+
 from pydantic.schema import schema
 
 from python_libs.pydantic.models import Vendor, Receipt, Customer, Account
@@ -6,5 +8,6 @@ if __name__ == '__main__':
     models_schema = schema([Vendor, Receipt, Customer, Account])
 
     # Export to a JSON file
-    with open('models_schema.json', 'w') as f:
-        f.write(models_schema.json())
+    with open('../../tests/fixtures/models_schema.json', 'w') as f:
+        json.dump(models_schema, f, default=str)
+        # f.write(models_schema.json())
