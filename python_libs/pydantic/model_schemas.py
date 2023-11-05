0380612c664387b1baff71afcb140fe0cbbdda3c
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-05 12:14:30 -0500

    Schema validation

--- python_libs/pydantic/model_schemas.py
+++ python_libs/pydantic/model_schemas.py
@@ -1,13 +1,36 @@
 import json
 
+from jsonschema.exceptions import ValidationError
 from pydantic.schema import schema
 
 from python_libs.pydantic.models import Vendor, Receipt, Customer, Account
+
+
+def write_schemas(file: str, schemas: schema):
+    with open(file, 'w') as f:
+        json.dump(schemas, f, default=str)
+
+def validate(file: str):
+    # Load your JSON schema
+    with open(file, 'r') as schema_file:
+        schema = json.load(schema_file)
+
+    # Your dictionary that you want to validate
+    data_to_validate = {
+        # ... Your dictionary data here
+    }
+
+    # Validate the data
+    try:
+        validate(instance=data_to_validate, schema=schema)
+        print("Validation passed!")
+    except ValidationError as e:
+        print("Validation failed!")
+        print(e)
 
 if __name__ == '__main__':
     models_schema = schema([Vendor, Receipt, Customer, Account])
 
     # Export to a JSON file
-    with open('../../tests/fixtures/models_schema.json', 'w') as f:
-        json.dump(models_schema, f, default=str)
-        # f.write(models_schema.json())
+    schema_json_file =  '../../tests/fixtures/models_schema.json'
+    write_schemas(schema_json_file, models_schema)
