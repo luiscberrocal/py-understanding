11b2f43cc86cf4d92ddba24d00a17a2773fac315
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-09 18:29:12 -0500

    Made progress bar transient.

--- python_libs/internet_speed/check_speed.py
+++ python_libs/internet_speed/check_speed.py
@@ -1,3 +1,4 @@
+import os
 import random
 import time
 from datetime import datetime
@@ -10,7 +11,7 @@ from rich.live import Live
 from rich.progress import track
 from rich.spinner import Spinner
 from rich.text import Text
-from speedtest import Speedtest
+from speedtest import Speedtest, SpeedtestBestServerFailure
 
 from python_libs.internet_speed.events import Observer
 from python_libs.internet_speed.schemas import SpeedSample
@@ -30,13 +31,13 @@ def check(speed_test: Speedtest, verbose: bool) -> Tuple[float, float, float]:
     start_time = time.time()
 
     spinner = Spinner('dots3', text=Text('Checking download speed...', style='green'))
-    with Live(spinner):
+    with Live(spinner, transient=True):
         download_speed = bytes_to_mb(speed_test.download())
     if verbose:
         print(f"Your Download speed is {download_speed:.2f}")
 
     spinner = Spinner('dots3', text=Text('Checking upload speed...', style='green'))
-    with Live(spinner):
+    with Live(spinner, transient=True):
         upload_speed = bytes_to_mb(speed_test.upload())
     if verbose:
         print(f"Your Upload speed is    {upload_speed:.2f}")
@@ -44,8 +45,29 @@ def check(speed_test: Speedtest, verbose: bool) -> Tuple[float, float, float]:
     return download_speed, upload_speed, elapsed_time
 
 
+def load_environment_variables(environment_filename: str):
+    from dotenv import load_dotenv
+    from pathlib import Path
+
+    def find_envs_folder(current_dir: Path):
+        env_folder = current_dir / '.envs'
+        if env_folder.exists():
+            return env_folder
+        else:
+            return find_envs_folder(current_dir.parent)
+
+    environment_folder = find_envs_folder(Path(__file__).parent)
+    environment_file = environment_folder / environment_filename
+    print(f'{environment_file} {environment_file.exists()}')
+    load_dotenv(dotenv_path=environment_file)
+
+
 if __name__ == '__main__':
+    load_environment_variables('internet_speed_vars.txt')
+
     sp_test = Speedtest()
+    print(f'>>> {sp_test.best}')
+
     o_folder = Path(__file__).parent.parent.parent / "output"
     ts = datetime.now().strftime("%Y-%m-%d_%H%M%S")
     json_file = o_folder / f"speed_test_{ts}.json"
@@ -54,7 +76,7 @@ if __name__ == '__main__':
     observer = Observer(csv_file)
     # print(o_folder, o_folder.exists())
 
-    total_runs = 12
+    total_runs = int(os.getenv('INTERNET_SPEED_RUNS'))
     wait_minutes_max = 5
     for i in range(total_runs):
         sleep_seconds = int(60 * random.random() * wait_minutes_max)
@@ -68,9 +90,10 @@ if __name__ == '__main__':
             speed_sample = SpeedSample(**speed_result)
             observer.update(speed_sample)
             print('-' * 80)
-            for _ in track(range(sleep_seconds), description=f"Sleeping for {sleep_seconds / 60:.2f} minutes..."):
+            for _ in track(range(sleep_seconds), description=f"Sleeping for {sleep_seconds / 60:.2f} minutes...",
+                           transient=True):
                 time.sleep(1)  # Simulate work being done
-        except speedtest.SpeedtestBestServerFailure as e:
+        except SpeedtestBestServerFailure as e:
             click.secho(f'Skipped {i} {e}', fg='red')
     # with open(json_file, "a") as f:
     #    json.dump(test_list, f)
