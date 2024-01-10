7986c5e380931b4d6d3806e73befa1add2b8317f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-09 19:43:37 -0500

    Added machine to environment variable.

--- python_libs/internet_speed/README.md
+++ python_libs/internet_speed/README.md
@@ -6,3 +6,8 @@
 python -m python_libs.internet_speed.check_speed
 ```
 
+## Prepare for developtment
+
+```shell
+pip install python_libs/internet_speed/requirements.txt
+```
--- python_libs/internet_speed/check_speed.py
+++ python_libs/internet_speed/check_speed.py
@@ -21,10 +21,10 @@ class SpeedTestResult(BaseModel):
     pass
 
 
-def bytes_to_mb(bytes: float) -> float:
-    KB = 1024  # One Kilobyte is 1024 bytes
-    MB = KB * 1024  # One MB is 1024 KB
-    return bytes / MB
+def bytes_to_mb(size_bytes: float) -> float:
+    kb = 1024  # One Kilobyte is 1024 bytes
+    mb = kb * 1024  # One MB is 1024 KB
+    return size_bytes / mb
 
 
 def check(speed_test: Speedtest, verbose: bool) -> Tuple[float, float, float]:
@@ -58,15 +58,26 @@ def load_environment_variables(environment_filename: str):
 
     environment_folder = find_envs_folder(Path(__file__).parent)
     environment_file = environment_folder / environment_filename
-    print(f'{environment_file} {environment_file.exists()}')
     load_dotenv(dotenv_path=environment_file)
+
+
+def getting_best_server():
+    spinner = Spinner('dots3', text=Text('Instantiating test...', style='green'))
+    with Live(spinner, transient=True):
+        speed_test = Speedtest()
+    spinner = Spinner('dots3', text=Text('Checking best server...', style='green'))
+    with Live(spinner, transient=True):
+        best = speed_test.best
+    print(f'Sponsor: {best["sponsor"]}')
+    print(f'Latency: {best["latency"]}')
+
+    return speed_test
 
 
 if __name__ == '__main__':
     load_environment_variables('internet_speed_vars.txt')
 
-    sp_test = Speedtest()
-    print(f'>>> {sp_test.best}')
+    sp_test = getting_best_server()
 
     o_folder = Path(__file__).parent.parent.parent / "output"
     ts = datetime.now().strftime("%Y-%m-%d_%H%M%S")
@@ -77,7 +88,8 @@ if __name__ == '__main__':
     # print(o_folder, o_folder.exists())
 
     total_runs = int(os.getenv('INTERNET_SPEED_RUNS'))
-    wait_minutes_max = 5
+    wait_minutes_max = int(os.getenv('INTERNET_SPEED_WAIT_MAX'))
+    machine_name = os.getenv('INTERNET_SPEED_MACHINE')
     for i in range(total_runs):
         sleep_seconds = int(60 * random.random() * wait_minutes_max)
         try:
@@ -85,7 +97,7 @@ if __name__ == '__main__':
             print(f"{i} Test took: {results[2]:.2f} seconds")
 
             now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            speed_result = {"machine": "Dell", "download": results[0], "upload": results[1],
+            speed_result = {"machine": machine_name, "download": results[0], "upload": results[1],
                             "elapsed_time": results[2], "date": now}
             speed_sample = SpeedSample(**speed_result)
             observer.update(speed_sample)
