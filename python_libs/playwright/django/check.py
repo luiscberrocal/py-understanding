da5d8fe39f623a28cb38987a288b86743c72a15b
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-05-17 11:59:44 -0500

    Check admin resonse

--- python_libs/playwright/django/check.py
+++ python_libs/playwright/django/check.py
@@ -1,4 +1,21 @@
+from playwright.sync_api import sync_playwright
+
+from python_libs.playwright.django.config import get_configuration, get_all_configurations
+from python_libs.playwright.django.schemas import AdminConfigSchema
 
 
-def check_admin():
-    pass
+def check_admin(config_scheme: AdminConfigSchema):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(config_scheme.admin_url)
+        print(page.title())
+
+
+if __name__ == '__main__':
+    # admin_config_schema = get_configuration('DLOC', 'production')
+    # check_admin(admin_config_schema)
+    admin_config_schemas = get_all_configurations('production')
+
+    for admin_config_schema in admin_config_schemas:
+        check_admin(admin_config_schema)
--- python_libs/playwright/django/config.py
+++ python_libs/playwright/django/config.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 
 from python_libs.playwright.django.constants import ENV_FOLDER
 from python_libs.playwright.django.schemas import AdminConfigSchema
@@ -12,6 +13,19 @@ def get_configuration(service_key: str, environment: str) -> AdminConfigSchema:
     config_data['service_key'] = service_key
     admin_config = AdminConfigSchema(**config_data)
     return admin_config
+
+
+def get_all_configurations(environment: str) -> List[AdminConfigSchema]:
+    configurations = []
+    configuration_file = ENV_FOLDER / f"django_admin_{environment}_config.json"
+    with open(configuration_file) as json_file:
+        configuration = json.load(json_file)
+    for key, value in configuration.items():
+        value['service_key'] = key
+        admin_config = AdminConfigSchema(**value)
+        configurations.append(admin_config)
+
+    return configurations
 
 
 if __name__ == '__main__':
