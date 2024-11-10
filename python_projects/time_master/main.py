3e71e648c8d02ff3b00b277d2581bb7ac6da3331
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-11-10 07:50:31 -0500

    Optimized code.

--- python_projects/time_master/main.py
+++ python_projects/time_master/main.py
@@ -2,75 +2,52 @@
 
 import csv
 import json
+import os
 import subprocess
 import sys
 from datetime import datetime
 from typing import Any
 
 
-def fetch_timewarrior_data():
+def fetch_data(command: list[str]) -> list[dict[str, Any]]:
     try:
-        timew_output = subprocess.check_output(
-            ["timew", "export"], universal_newlines=True
-        )
-        timew_data = json.loads(timew_output)
-        return timew_data
+        output = subprocess.check_output(command, universal_newlines=True)
+        return json.loads(output)
     except subprocess.CalledProcessError as e:
-        print(f"Error fetching TimeWarrior data: {e}")
+        print(f"Error fetching data with command {command}: {e}")
         sys.exit(1)
 
 
-def fetch_taskwarrior_data():
-    try:
-        task_output = subprocess.check_output(
-            ["task", "export"], universal_newlines=True
-        )
-        task_data = json.loads(task_output)
-        return task_data
-    except subprocess.CalledProcessError as e:
-        print(f"Error fetching TaskWarrior data: {e}")
-        sys.exit(1)
+def convert_to_datetime(date_str: str) -> datetime:
+    for fmt in ("%Y%m%dT%H%M%SZ", "%Y-%m-%d %H:%M:%S"):
+        try:
+            return datetime.strptime(date_str, fmt)
+        except ValueError:
+            continue
+    raise ValueError(f"Date string {date_str} is not in a recognized format")
 
 
-def convert_to_datetime(date_str):
-    try:
-        return datetime.strptime(date_str, "%Y%m%dT%H%M%SZ")
-    except ValueError:
-        return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
-
-
-# Example usage
-
-
-def parse_task_entries(
-        *, time_data: list[dict[str, Any]], task_data: list[dict[str, Any]]
-) -> list[dict[str, Any]]:
+def parse_task_entries(time_data: list[dict[str, Any]], task_data: list[dict[str, Any]]) -> list[dict[str, Any]]:
     time_data_with_tasks = []
     for task in task_data:
         for interval in time_data:
             try:
-                if (
-                        task["project"] in interval.get("tags", [])
-                        and interval.get("duration") is None
-                ):
+                if task["project"] in interval.get("tags", []) and interval.get("duration") is None:
                     start_time = convert_to_datetime(interval["start"])
-                    if interval.get("end"):
-                        end_time = convert_to_datetime(interval["end"])
-                        duration = end_time - start_time
-                    else:
-                        end_time = None
-                        duration = None
+                    end_time = convert_to_datetime(interval["end"]) if interval.get("end") else None
+                    duration = (end_time - start_time) if end_time else None
 
-                    interval["project"] = task["project"]
-                    interval["description"] = task["description"]
-                    interval["uuid"] = task["uuid"]
-                    interval["start"] = start_time.strftime("%Y-%m-%d %H:%M:%S")
-                    if end_time is not None:
-                        interval["end"] = end_time.strftime("%Y-%m-%d %H:%M:%S")
-                    interval["duration"] = str(duration)
-                    print(interval)
-                    print("-" * 120)
-                    if duration is not None:
+                    interval.update({
+                        "project": task["project"],
+                        "description": task["description"],
+                        "uuid": task["uuid"],
+                        "start": start_time.strftime("%Y-%m-%d %H:%M:%S"),
+                        "end": end_time.strftime("%Y-%m-%d %H:%M:%S") if end_time else None,
+                        "duration": str(duration) if duration else None,
+                        "user": os.getenv("USER")
+                    })
+
+                    if duration:
                         time_data_with_tasks.append(interval)
                     else:
                         print(f">>> Skipping entry without end time: {interval}")
@@ -80,17 +57,13 @@ def parse_task_entries(
     return time_data_with_tasks
 
 
-def write_csv(
-        time_entries: list[dict[str, Any]], filename: str = "timew_taskw_report.csv"
-):
-    fieldnames = ["Task UUID", "Description", "Start", "End", "Duration (HH:MM:SS)"]
+def write_csv(time_entries: list[dict[str, Any]], filename: str = "timew_taskw_report.csv"):
     fieldnames = time_entries[0].keys()
     try:
         with open(filename, mode="w", newline="") as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
-            for entry in time_entries:
-                writer.writerow(entry)
+            writer.writerows(time_entries)
         print(f"Report successfully written to {filename}")
     except IOError as e:
         print(f"Error writing to CSV file: {e}")
@@ -98,8 +71,8 @@ def write_csv(
 
 
 def main():
-    timew_data = fetch_timewarrior_data()
-    task_data = fetch_taskwarrior_data()
+    timew_data = fetch_data(["timew", "export"])
+    task_data = fetch_data(["task", "export"])
     entries = parse_task_entries(time_data=timew_data, task_data=task_data)
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     report_file = f"timew_taskw_report_{timestamp}.csv"
